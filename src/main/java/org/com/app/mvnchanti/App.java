f7f85901a51c877edddcc59c9bbe7549997d542b
mohan <mohan.chanti4@gmail.com>
2019-07-19 15:30:14 +0530

    new changes

---
+++ src/main/java/org/com/app/mvnchanti/App.java
@@ -0,0 +1,13 @@
+package org.com.app.mvnchanti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "mvnchanti" );
+    }
+}
